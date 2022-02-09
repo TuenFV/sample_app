d92ef34a4963c92fb160a927156f8b6f602f8cfe
LoKi <sand5088@gmail.com>
2022-02-09 17:00:23 +0700

    Implement advanced login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
@@ -9,6 +7,9 @@ Rails.application.routes.draw do
   
   get '/login',      to: 'sessions#new'
   post '/login',     to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get '/logout',  to: 'sessions#destroy'
+  
   resources :users
+
+  root 'static_pages#home'
 end
